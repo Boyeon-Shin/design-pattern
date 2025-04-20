d55cc1435aacac8df537d851c7a5aa10f1b101cb
Boyeon-Shin <anna007096@gmail.com>
2025-04-20 14:54:59 +0900

    4.팩토리 패턴

--- src/ch04/pizza/PizzaStore.java
+++ src/ch04/pizza/PizzaStore.java
@@ -1,5 +1,24 @@
 package ch04.pizza;
 
 public class PizzaStore {
+    SimplePizzaFactory factory;
+
+    public PizzaStore(SimplePizzaFactory factory) {
+        this.factory = factory;
+    }
+
+    Pizza OrderPizza(String type) {
+        Pizza pizza;
+
+        pizza = factory.createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+
 
 }
