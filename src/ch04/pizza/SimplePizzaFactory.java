3bd1b31edf429f6a2d041d0b62ce099622111855
Boyeon-Shin <anna007096@gmail.com>
2025-04-21 23:45:00 +0900

    4.팩토리 패턴

--- src/ch04/pizza/SimplePizzaFactory.java
+++ src/ch04/pizza/SimplePizzaFactory.java
@@ -6,8 +6,8 @@ public class SimplePizzaFactory {
 
         if (type.equals("cheese")) {
             pizza = new CheesePizza();
-        } else if (type.equals("greek")) {
-            pizza = new GreekPizza();
+        } else if (type.equals("pepperoni")) {
+            pizza = new PepperoniPizza();
         } else if (type.equals("clam")) {
             pizza = new ClamPizza();
         } else if (type.equals("veggie")) {
