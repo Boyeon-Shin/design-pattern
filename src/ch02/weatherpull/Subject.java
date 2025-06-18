18bc137fd9831e0c63962fceb9de3484be44327b
Boyeon-Shin <anna007096@gmail.com>
2025-06-18 14:25:37 +0900

    observer 패턴

--- src/ch02/weatherpull/Subject.java
+++ src/ch02/weatherpull/Subject.java
@@ -1,8 +1,9 @@
 package ch02.weatherpull;
 
 public interface Subject {
-    public void registerObserver(Observer observer);
-    public void removeObserver(Observer observer);
-    //옵저버의
-    public void notifyObservers();
+    void registerObserver(Observer observer);
+
+    void removeObserver(Observer observer);
+
+    void notifyObservers();
 }
